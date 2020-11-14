3f8689ee138faa1c82e3f0d7cc321a3c47e48141
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-14 15:29:08 -0600

    Adds tolower preprocessor

--- pandas_preprocessor/preprocessors/__init__.py
+++ pandas_preprocessor/preprocessors/__init__.py
@@ -2,11 +2,13 @@ from pandas_preprocessor.preprocessors.substitution import Substitution
 from pandas_preprocessor.preprocessors.to_num import ToNum
 from pandas_preprocessor.preprocessors.to_type import ToType
 from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
+from pandas_preprocessor.preprocessors.to_lower import ToLower
 
 preprocessors = {
     'substitution': lambda column, dataframe, settings: Substitution(column, dataframe, settings),
     'to_num': lambda column, dataframe, settings: ToNum(column, dataframe, settings),
-    'to_type': lambda column, dataframe, settings: ToType(column, dataframe, settings)
+    'to_type': lambda column, dataframe, settings: ToType(column, dataframe, settings),
+    'to_lower': lambda column, dataframe, settings: ToLower(column, dataframe, settings)
 }
 
 
---
+++ pandas_preprocessor/preprocessors/to_lower.py
@@ -0,0 +1,17 @@
+import re
+from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
+from functools import reduce
+
+
+class ToLower(APreprocessor):
+
+    def __init__(self, column, dataframe, settings):
+        APreprocessor.__init__(self, column, dataframe, settings)
+
+    def transform(self, dataframe):
+        dataframe[self.column] = dataframe[self.column].map(
+            lambda string: string.lower())
+        return dataframe
+
+    def invert_transform(self, dataframe):
+        return dataframe

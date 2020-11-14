3f15a499841a076d1c50eb236dfc035a9ec30197
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-14 15:33:41 -0600

    Adds to upper preprocessor

--- pandas_preprocessor/preprocessors/__init__.py
+++ pandas_preprocessor/preprocessors/__init__.py
@@ -3,12 +3,14 @@ from pandas_preprocessor.preprocessors.to_num import ToNum
 from pandas_preprocessor.preprocessors.to_type import ToType
 from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
 from pandas_preprocessor.preprocessors.to_lower import ToLower
+from pandas_preprocessor.preprocessors.to_upper import ToUpper
 
 preprocessors = {
     'substitution': lambda column, dataframe, settings: Substitution(column, dataframe, settings),
     'to_num': lambda column, dataframe, settings: ToNum(column, dataframe, settings),
     'to_type': lambda column, dataframe, settings: ToType(column, dataframe, settings),
-    'to_lower': lambda column, dataframe, settings: ToLower(column, dataframe, settings)
+    'to_lower': lambda column, dataframe, settings: ToLower(column, dataframe, settings),
+    'to_upper': lambda column, dataframe, settings: ToUpper(column, dataframe, settings)
 }
 
 
--- pandas_preprocessor/preprocessors/to_upper.py
+++ pandas_preprocessor/preprocessors/to_upper.py
@@ -1,14 +1,14 @@
 from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
 
 
-class ToLower(APreprocessor):
+class ToUpper(APreprocessor):
 
     def __init__(self, column, dataframe, settings):
         APreprocessor.__init__(self, column, dataframe, settings)
 
     def transform(self, dataframe):
         dataframe[self.column] = dataframe[self.column].map(
-            lambda string: string.lower())
+            lambda string: string.upper())
         return dataframe
 
     def invert_transform(self, dataframe):

982d27985ce5934fe9f70e6ebf279628e021f56c
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-14 16:47:01 -0600

    Adds mv_mode preprocessor

--- pandas_preprocessor/preprocessors/__init__.py
+++ pandas_preprocessor/preprocessors/__init__.py
@@ -4,13 +4,15 @@ from pandas_preprocessor.preprocessors.to_type import ToType
 from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
 from pandas_preprocessor.preprocessors.to_lower import ToLower
 from pandas_preprocessor.preprocessors.to_upper import ToUpper
+from pandas_preprocessor.preprocessors.mv_mode import MvMode
 
 preprocessors = {
     'substitution': lambda column, dataframe, settings: Substitution(column, dataframe, settings),
     'to_num': lambda column, dataframe, settings: ToNum(column, dataframe, settings),
     'to_type': lambda column, dataframe, settings: ToType(column, dataframe, settings),
     'to_lower': lambda column, dataframe, settings: ToLower(column, dataframe, settings),
-    'to_upper': lambda column, dataframe, settings: ToUpper(column, dataframe, settings)
+    'to_upper': lambda column, dataframe, settings: ToUpper(column, dataframe, settings),
+    'mv_mode': lambda column, dataframe, settings: MvMode(column, dataframe, settings)
 }
 
 
---
+++ pandas_preprocessor/preprocessors/mv_mode.py
@@ -0,0 +1,15 @@
+from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
+
+
+class MvMode(APreprocessor):
+
+    def __init__(self, column, dataframe, settings):
+        APreprocessor.__init__(self, column, dataframe, settings)
+
+    def transform(self, dataframe):
+        x = dataframe[self.column].mode()[0]
+        dataframe[self.column] = dataframe[self.column].fillna(x)
+        return dataframe
+
+    def invert_transform(self, dataframe):
+        return dataframe

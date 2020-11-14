afa859c96f45e962639213555ee84055a8e8ce54
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-14 16:39:50 -0600

    Adds should_invert_flag to one hot encoder

--- pandas_preprocessor/encoders/one_hot_encoder.py
+++ pandas_preprocessor/encoders/one_hot_encoder.py
@@ -9,18 +9,21 @@ class OneHotEncoder(AEncoder):
         AEncoder.__init__(self, column, dataframe, settings)
         self.og_column = None
         self.added_columns = []
+        self.shouldTrackInvert = self.settings.get('should_track_invert', True)
 
     def transform(self, dataframe):
         cs = pd.get_dummies(
             dataframe[self.column], prefix=self.column)
-        for c in cs.columns:
-            self.added_columns.append(c)
-        self.og_column = dataframe[self.column]
+        if(self.shouldTrackInvert):
+            for c in cs.columns:
+                self.added_columns.append(c)
+            self.og_column = dataframe[self.column]
         dataframe = pd.concat([dataframe, cs], axis=1)
         dataframe.drop(self.column, inplace=True, axis=1)
         return dataframe
 
     def invert_transform(self, dataframe):
-        dataframe[self.column] = self.og_column
-        dataframe.drop(self.added_columns, inplace=True, axis=1)
+        if(self.shouldTrackInvert):
+            dataframe[self.column] = self.og_column
+            dataframe.drop(self.added_columns, inplace=True, axis=1)
         return dataframe

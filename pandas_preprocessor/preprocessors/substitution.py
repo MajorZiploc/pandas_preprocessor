1f7bf326aa0b8ee4b4c2f98669d210dd49dce0d4
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-14 14:27:58 -0600

    Improves substitution preprocessor

--- pandas_preprocessor/preprocessors/substitution.py
+++ pandas_preprocessor/preprocessors/substitution.py
@@ -1,5 +1,6 @@
 import re
 from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
+from functools import reduce
 
 
 class Substitution(APreprocessor):
@@ -7,9 +8,33 @@ class Substitution(APreprocessor):
     def __init__(self, column, dataframe, settings):
         APreprocessor.__init__(self, column, dataframe, settings)
 
+        self.flag_map = {
+            "i": re.IGNORECASE,
+            "x": re.VERBOSE,
+            "m": re.MULTILINE,
+            "s": re.DOTALL,
+            "u": re.UNICODE,
+            "l": re.LOCALE,
+            "d": re.DEBUG,
+            "a": re.ASCII,
+            "t": re.TEMPLATE
+        }
+
     def transform(self, dataframe):
-        dataframe[self.column] = dataframe[self.column].map(
-            lambda string: re.sub(self.settings['pattern'], self.settings['replace'], string, flags=re.I))
+        def fnWithFlags(string):
+            def r(acc, ele):
+                acc = acc | ele
+                return acc
+            flags = self.settings.get('flags').lower()
+            return re.sub(self.settings['pattern'], self.settings['replace'], string, flags=reduce(r, [self.flag_map[f] for f in flags]))
+
+        def fnWithoutFlags(string):
+            return re.sub(self.settings['pattern'], self.settings['replace'], string)
+
+        fn = fnWithFlags if self.settings.get('flags') else fnWithoutFlags
+
+        flags = self.settings.get('flags')
+        dataframe[self.column] = dataframe[self.column].map(fn)
         return dataframe
 
     def invert_transform(self, dataframe):

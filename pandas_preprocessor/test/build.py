8f0e2aba43ca04443c882630e8cfabc80dd66df8
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-14 07:53:41 -0600

    Edits test case

--- pandas_preprocessor/test/build.py
+++ pandas_preprocessor/test/build.py
@@ -4,20 +4,27 @@ import toml
 from pandas_preprocessor import *
 import os
 
-this_dir = os.path.dirname(os.path.realpath(__file__))
-tomlLoc = os.path.join(this_dir, "config.toml")
-print(tomlLoc)
-config = toml.load(tomlLoc)
-fileLoc = os.path.join(this_dir, config['data']['connectionstring'])
-print(fileLoc)
-config['data']['connectionstring'] = fileLoc
 
-df = get_dataframe(config['data'])
+def l(df): return [df.head(), df.dtypes, df.shape, df.columns, df.index]
 
-# l = [df.head(), df.dtypes, df.shape, df.columns, df.index]
 
-# foreach(print, l)
-print(df.head())
-cleaned_df = clean_dataframe(df, config['dataframe'])
-print(cleaned_df.head())
-print(invert_cleaning(cleaned_df, config['dataframe']).head())
+def build():
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    tomlLoc = os.path.join(this_dir, "config.toml")
+    print(tomlLoc)
+    config = toml.load(tomlLoc)
+    fileLoc = os.path.join(this_dir, config['data']['connectionstring'])
+    print(fileLoc)
+    config['data']['connectionstring'] = fileLoc
+
+    df = get_dataframe(config['data'])
+
+    # foreach(print, l(df))
+    cleaned_df = clean_dataframe(df, config['dataframe'])
+    # cleaned_df['Grass'] = pd.to_numeric(cleaned_df['Grass'], downcast=None)
+    # cleaned_df['Grass'] = cleaned_df['Grass'].astype('object')
+    # foreach(print, l(cleaned_df))
+    foreach(print, l(invert_cleaning(cleaned_df, config['dataframe'])))
+
+
+build()

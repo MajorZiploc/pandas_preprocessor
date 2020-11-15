785905c0672d4e6e963023e5ccc13e4eb127a1da
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-15 16:37:28 -0600

    Adds saving to joblib file for label encoder

--- pandas_preprocessor/encoders/label_encoder.py
+++ pandas_preprocessor/encoders/label_encoder.py
@@ -1,5 +1,6 @@
 from sklearn import preprocessing
 from pandas_preprocessor.encoders.aencoder import AEncoder
+import joblib
 
 
 class LabelEncoder(AEncoder):
@@ -7,23 +8,17 @@ class LabelEncoder(AEncoder):
     def __init__(self, column, dataframe, settings):
         AEncoder.__init__(self, column, dataframe, settings)
         self.labelEncoder = preprocessing.LabelEncoder()
-        self.set_label_encoder(dataframe)
-
-    def set_label_encoder(self, dataframe):
         self.labelEncoder.fit(dataframe[self.column])
+        joblib_file = settings.get('file_location')
+        if(joblib_file is not None):
+            joblib.dump(self.labelEncoder, joblib_file)
 
-    def label_encode_transform(self, dataframe):
+    def transform(self, dataframe):
         dataframe[self.column] = self.labelEncoder.transform(
             dataframe[self.column])
         return dataframe
 
-    def label_encode_inverse_transform(self, dataframe):
+    def invert_transform(self, dataframe):
         dataframe[self.column] = self.labelEncoder.inverse_transform(
             dataframe[self.column])
         return dataframe
-
-    def transform(self, dataframe):
-        return self.label_encode_transform(dataframe)
-
-    def invert_transform(self, dataframe):
-        return self.label_encode_inverse_transform(dataframe)

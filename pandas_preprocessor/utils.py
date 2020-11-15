13681618dfc6e7962e0c291bacfcd47246080ae3
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-15 16:35:37 -0600

    Adds none chain util fn

--- pandas_preprocessor/utils.py
+++ pandas_preprocessor/utils.py
@@ -1,3 +1,10 @@
 def foreach(action, iterable):
     for element in iterable:
         action(element)
+
+
+def nc(supplier):
+    try:
+        v = supplier()
+    except AttributeError:
+        v = None

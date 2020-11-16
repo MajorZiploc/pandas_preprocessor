7d7455f5826d5d58e645486290ea5ef886c5bee6
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-16 15:51:31 -0600

    Fixes set_full_path to set data location

--- pandas_preprocessor/utils.py
+++ pandas_preprocessor/utils.py
@@ -15,9 +15,14 @@ def nc(supplier):
 
 
 def set_full_paths(config, directory):
-    config['model_file_location'] = os.path.join(directory, config['model_file_location'])
-    config['input_query']['connectionstring'] = os.path.join(
-        directory, config['input_query']['connectionstring'])
+    config['model_file_location'] = os.path.join(
+        directory, config['model_file_location'])
+    if(not nc(lambda: config['input_query']['format']) == 'sql'):
+        config['input_query']['connectionstring'] = os.path.join(
+            directory, config['input_query']['connectionstring'])
+    if(not nc(lambda: config['data']['format']) == 'sql'):
+        config['data']['connectionstring'] = os.path.join(
+            directory, config['data']['connectionstring'])
     inputs = config['dataframe']['inputs']
     for i in inputs:
         es = i.get('encoding_steps', [])
@@ -31,3 +36,7 @@ def load_model(config):
     model_file = config['model_file_location']
     model = joblib.load(model_file)
     return model
+
+# df1 = r
+# df2 = r.dropna(axis=0, how='any', thresh=None, subset=None)
+# df3 = df1[~df1.isin(df2)].dropna(how='all')

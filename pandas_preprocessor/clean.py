deb7ee72113e2a3a85e3d184fe4d5bc1b0f07cc5
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-16 15:04:19 -0600

    Adds is_use_case flag to clean.py

--- pandas_preprocessor/clean.py
+++ pandas_preprocessor/clean.py
@@ -9,21 +9,23 @@ def get_dataframe(dataConfig):
         else None
 
 
-def encodersAction(df, column):
+def encodersAction(df, column, is_use_case):
     es = column.get('encoding_steps')
     if(es is not None):
         for step in es:
-            df = encoderAction(df, step, column)
+            df = encoderAction(df, step, column, is_use_case)
     return df
 
 
-def encoderAction(df, step, column):
-    setEncoder(df, step, column)
+def encoderAction(df, step, column, is_use_case):
+    setEncoder(df, step, column, is_use_case)
     return encoderTransform(df, step)
 
 
-def setEncoder(df, step, column):
+def setEncoder(df, step, column, is_use_case):
     if(step is not None):
+        settings = step.get('settings')
+        settings['is_use_case'] = is_use_case
         step['encoder'] = encoder_selector(
             step['algo'])(column['name'], df, step.get('settings'))
 
@@ -48,21 +50,23 @@ def encoderInvert(df, step):
     return df
 
 
-def preprocessorsAction(df, column):
+def preprocessorsAction(df, column, is_use_case):
     ps = column.get('preprocess_steps')
     if(ps is not None):
         for step in ps:
-            df = preprocessorAction(df, step, column)
+            df = preprocessorAction(df, step, column, is_use_case)
     return df
 
 
-def preprocessorAction(df, step, column):
-    setPreprocessor(df, step, column)
+def preprocessorAction(df, step, column, is_use_case):
+    setPreprocessor(df, step, column, is_use_case)
     return preprocessorTransform(df, step)
 
 
-def setPreprocessor(df, step, column):
+def setPreprocessor(df, step, column, is_use_case):
     if(step is not None):
+        settings = step.get('settings')
+        settings['is_use_case'] = is_use_case
         step['preprocessor'] = preprocessor_selector(
             step['algo'])(column['name'], df, step.get('settings'))
 
@@ -87,7 +91,11 @@ def preprocessorInvert(df, step):
     return df
 
 
-def clean_dataframe(dataframe, dfConfig):
+def clean_input(dataframe, dfConfig):
+    return clean_dataframe(dataframe, dfConfig, is_use_case=True)
+
+
+def clean_dataframe(dataframe, dfConfig, is_use_case=False):
     inputs = dfConfig['inputs']
     outputs = dfConfig['outputs']
     dfConfig['keep_columns'] = dfConfig.get('keep_columns', True)
@@ -101,10 +109,10 @@ def clean_dataframe(dataframe, dfConfig):
         df = dataframe.copy()
 
     for c in specifiedColumn:
-        df = preprocessorsAction(df, c)
+        df = preprocessorsAction(df, c, is_use_case)
 
     for c in specifiedColumn:
-        df = encodersAction(df, c)
+        df = encodersAction(df, c, is_use_case)
 
     return df
 

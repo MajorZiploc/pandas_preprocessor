c6c86b4d0749e1bad8ca26c382da1730b7940258
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-15 19:36:02 -0600

    Adds label binarizer encoder

--- pandas_preprocessor/encoders/__init__.py
+++ pandas_preprocessor/encoders/__init__.py
@@ -1,9 +1,11 @@
 from pandas_preprocessor.encoders.label_encoder import LabelEncoder
 from pandas_preprocessor.encoders.one_hot_encoder import OneHotEncoder
+from pandas_preprocessor.encoders.label_binarizer import LabelBinarizer
 
 encoders = {
     'label_encoding': lambda column, dataframe, settings: LabelEncoder(column, dataframe, settings),
-    'one_hot_encoding': lambda column, dataframe, settings: OneHotEncoder(column, dataframe, settings)
+    'one_hot_encoding': lambda column, dataframe, settings: OneHotEncoder(column, dataframe, settings),
+    'label_binarizer': lambda column, dataframe, settings: LabelBinarizer(column, dataframe, settings)
 }
 
 
--- pandas_preprocessor/encoders/label_binarizer.py
+++ pandas_preprocessor/encoders/label_binarizer.py
@@ -8,7 +8,7 @@ class LabelBinarizer(AEncoder):
 
     def __init__(self, column, dataframe, settings):
         AEncoder.__init__(self, column, dataframe, settings)
-        self.labelBinarizer = preprocessing.LabelBinarizer(neg_label=0)
+        self.labelBinarizer = preprocessing.LabelBinarizer()
         self.labelBinarizer.fit(dataframe[self.column].to_frame())
         joblib_file = settings.get('file_location')
         if(joblib_file is not None):
@@ -16,11 +16,18 @@ class LabelBinarizer(AEncoder):
 
     def transform(self, dataframe):
         x = self.labelBinarizer.transform(dataframe[self.column].to_frame())
-        cs = self.labelBinarizer.transform(dataframe[self.column])
-        dataframe = pd.concat([dataframe, pd.DataFrame(cs)], axis=1)
+        cs = pd.DataFrame(self.labelBinarizer.transform(dataframe[self.column]))\
+            .add_prefix(self.column+"_")
+        dataframe = pd.concat([dataframe, cs], axis=1)
+        dataframe.drop(self.column, inplace=True, axis=1)
         return dataframe
 
     def invert_transform(self, dataframe):
-        # dataframe[self.column] = self.labelBinarizer.inverse_transform(
-        #     dataframe[self.column])
+        waterCols = dataframe.filter(regex='^'+self.column+'_', axis=1)
+        x = self.labelBinarizer.inverse_transform(
+            waterCols.to_numpy())
+        waterDf = pd.DataFrame(x).rename({0: self.column}, axis=1)
+        dataframe.drop(waterCols, inplace=True, axis=1)
+        dataframe = pd.merge(dataframe, waterDf,
+                             left_index=True, right_index=True)
         return dataframe

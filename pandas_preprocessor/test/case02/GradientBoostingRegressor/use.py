e0b0dc2945beebb35f23eab14df9604d5b50693d
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-16 15:29:47 -0600

    Refactors testcase02.use and creates utils

--- pandas_preprocessor/test/case02/GradientBoostingRegressor/use.py
+++ pandas_preprocessor/test/case02/GradientBoostingRegressor/use.py
@@ -1,48 +1,42 @@
-import joblib
 from pandas_preprocessor import *
 import os
 import toml
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-tomlLoc = os.path.join(this_dir, "config.toml")
+config_name = "config.toml"
+
+# Loads config
+tomlLoc = os.path.join(this_dir, config_name)
 config = toml.load(tomlLoc)
-joblib_file = os.path.join(this_dir, config['model_file_location'])
-# Load from file
-model = joblib.load(joblib_file)
 
-inputs = config['dataframe']['inputs']
+# prefix all file locations with this directory
+set_full_paths(config, this_dir)
+# Loads pickled model
+model = load_model(config)
 
-for i in inputs:
-    es = i.get('encoding_steps', [])
-    for e in es:
-        fl = nc(lambda: e['settings']['file_location'])
-        if (fl is not None):
-            e['settings']['file_location'] = os.path.join(this_dir, fl)
-
-config['input_query']['connectionstring'] = os.path.join(
-    this_dir, config['input_query']['connectionstring'])
-
-
+# get user query
 df = get_dataframe(config['input_query'])
+
+# clean user query for models consumption
 house_data = df.drop(['Address', 'Method', 'SellerG', 'Date',
                       'Postcode', 'Lattitude', 'Longtitude', 'Regionname', 'Propertycount'], axis=1)
-
-house_data = clean_input(house_data, dfConfig=config['dataframe'])
+house_data = clean_input(house_data, config['dataframe'])
+outputColumnNames = [o['name'] for o in config['dataframe']['outputs']]
+house_to_value = house_data.drop(outputColumnNames, axis=1)
 
 print('Transformed DF')
 print(house_data.head())
 
-house_to_value = house_data.drop(['Price'], axis=1)
-
+# ask the model the user query
 predicted_house_value = model.predict(house_to_value)
 
 predicted_house_value = predicted_house_value[0]
 
 # Print estimated value of the property to two decimal places
-
 print("This property has an estimated value of AUD %.2f" %
       predicted_house_value)
 
+# Transform user query back to users data structure
 inverted_house_data = invert_cleaning(house_data, config['dataframe'])
 
 print('Inverted DF')
--- pandas_preprocessor/utils.py
+++ pandas_preprocessor/utils.py
@@ -1,3 +1,7 @@
+import os
+import joblib
+
+
 def foreach(action, iterable):
     for element in iterable:
         action(element)
@@ -8,3 +12,22 @@ def nc(supplier):
         return supplier()
     except AttributeError:
         return None
+
+
+def set_full_paths(config, directory):
+    config['model_file_location'] = os.path.join(directory, config['model_file_location'])
+    config['input_query']['connectionstring'] = os.path.join(
+        directory, config['input_query']['connectionstring'])
+    inputs = config['dataframe']['inputs']
+    for i in inputs:
+        es = i.get('encoding_steps', [])
+        for e in es:
+            fl = nc(lambda: e['settings']['file_location'])
+            if (fl is not None):
+                e['settings']['file_location'] = os.path.join(directory, fl)
+
+
+def load_model(config):
+    model_file = config['model_file_location']
+    model = joblib.load(model_file)
+    return model

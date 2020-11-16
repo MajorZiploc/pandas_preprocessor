b2ed7085bc5deab4ab5f0d3612f59aadb91377e4
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-16 10:04:26 -0600

    Fixes label binarizer

--- pandas_preprocessor/encoders/label_binarizer.py
+++ pandas_preprocessor/encoders/label_binarizer.py
@@ -9,7 +9,8 @@ class LabelBinarizer(AEncoder):
     def __init__(self, column, dataframe, settings):
         AEncoder.__init__(self, column, dataframe, settings)
         self.encoder = preprocessing.LabelBinarizer()
-        self.encoder.fit(dataframe[self.column].to_frame())
+        c = dataframe[self.column].to_frame()
+        self.encoder.fit(c)
         joblib_file = settings.get('file_location')
         if(joblib_file is not None):
             joblib.dump(self.encoder, joblib_file)
@@ -18,16 +19,15 @@ class LabelBinarizer(AEncoder):
         x = self.encoder.transform(dataframe[self.column].to_frame())
         cs = pd.DataFrame(self.encoder.transform(dataframe[self.column]))\
             .add_prefix(self.column+"_")
-        dataframe = pd.concat([dataframe, cs], axis=1)
+        dataframe = dataframe.join(cs)
         dataframe.drop(self.column, inplace=True, axis=1)
         return dataframe
 
     def invert_transform(self, dataframe):
-        waterCols = dataframe.filter(regex='^'+self.column+'_', axis=1)
+        binCols = dataframe.filter(regex='^'+self.column+'_', axis=1)
         x = self.encoder.inverse_transform(
-            waterCols.to_numpy())
-        waterDf = pd.DataFrame(x).rename({0: self.column}, axis=1)
-        dataframe.drop(waterCols, inplace=True, axis=1)
-        dataframe = pd.merge(dataframe, waterDf,
-                             left_index=True, right_index=True)
+            binCols.to_numpy())
+        binDf = pd.DataFrame(x).rename({0: self.column}, axis=1)
+        dataframe.drop(binCols, inplace=True, axis=1)
+        dataframe = dataframe.join(binDf)
         return dataframe

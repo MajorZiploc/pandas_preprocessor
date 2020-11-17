69f6ed79ada802a511abc2e7ad5bab090a98e066
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-17 12:00:21 -0600

    Abstracts out common pickle process in preprocessors

--- pandas_preprocessor/preprocessors/apreprocessor.py
+++ pandas_preprocessor/preprocessors/apreprocessor.py
@@ -1,12 +1,25 @@
+import joblib
+
 
 class APreprocessor(object):
 
     def __init__(self, column, dataframe, settings):
         self.column = column
         self.settings = settings if settings is not None else {}
+        self.scaler = None
 
     def transform(self, dataframe):
         return dataframe
 
     def invert_transform(self, dataframe):
         return dataframe
+
+    def pickle_process(self, dataframe):
+        joblib_file = self.settings.get('file_location')
+        if(self.settings.get('is_use_case', False)):
+            self.scaler = joblib.load(joblib_file)
+        else:
+            c = dataframe[self.column].to_frame()
+            self.scaler.fit(c)
+            if(joblib_file is not None):
+                joblib.dump(self.scaler, joblib_file)
--- pandas_preprocessor/preprocessors/normalize_min_max_scaler.py
+++ pandas_preprocessor/preprocessors/normalize_min_max_scaler.py
@@ -1,7 +1,6 @@
 from sklearn import preprocessing
 from pandas_preprocessor.preprocessors.apreprocessor import APreprocessor
 import pandas as pd
-import joblib
 
 
 class NormalizeMinMaxScaler(APreprocessor):
@@ -9,14 +8,7 @@ class NormalizeMinMaxScaler(APreprocessor):
     def __init__(self, column, dataframe, settings):
         APreprocessor.__init__(self, column, dataframe, settings)
         self.scaler = preprocessing.MinMaxScaler()
-        joblib_file = settings.get('file_location')
-        if(self.settings.get('is_use_case', False)):
-            self.scaler = joblib.load(joblib_file)
-        else:
-            c = dataframe[self.column].to_frame()
-            self.scaler.fit(c)
-            if(joblib_file is not None):
-                joblib.dump(self.scaler, joblib_file)
+        self.pickle_process(dataframe)
 
     def transform(self, dataframe):
         dataframe[self.column] = self.scaler.transform(

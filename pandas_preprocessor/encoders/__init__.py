39893c3c3308023dc4046441daedca41f9004481
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-18 07:52:22 -0600

    Removes one_hot_encoder

--- pandas_preprocessor/encoders/__init__.py
+++ pandas_preprocessor/encoders/__init__.py
@@ -1,11 +1,9 @@
 from pandas_preprocessor.encoders.label_encoder import LabelEncoder
-from pandas_preprocessor.encoders.one_hot_encoder import OneHotEncoder
 from pandas_preprocessor.encoders.label_binarizer import LabelBinarizer
 from pandas_preprocessor.encoders.kbins_discretizer import KBinsDiscretizer
 
 encoders = {
     'label_encoder': lambda column, dataframe, settings: LabelEncoder(column, dataframe, settings),
-    'one_hot_encoder': lambda column, dataframe, settings: OneHotEncoder(column, dataframe, settings),
     'label_binarizer': lambda column, dataframe, settings: LabelBinarizer(column, dataframe, settings),
     'kbins_discretizer': lambda column, dataframe, settings: KBinsDiscretizer(column, dataframe, settings)
 }

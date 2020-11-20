702b78beb1608cd723c33751c07469b045595786
Manyu Lakhotia <lakemonyew@gmail.com>
2020-11-20 06:26:03 -0600

    Removes unneeded lambda wrappers

--- pandas_preprocessor/encoders/__init__.py
+++ pandas_preprocessor/encoders/__init__.py
@@ -3,9 +3,9 @@ from pandas_preprocessor.encoders.label_binarizer import LabelBinarizer
 from pandas_preprocessor.encoders.kbins_discretizer import KBinsDiscretizer
 
 encoders = {
-    'label_encoder': lambda column, dataframe, settings: LabelEncoder(column, dataframe, settings),
-    'label_binarizer': lambda column, dataframe, settings: LabelBinarizer(column, dataframe, settings),
-    'kbins_discretizer': lambda column, dataframe, settings: KBinsDiscretizer(column, dataframe, settings)
+    'label_encoder': LabelEncoder,
+    'label_binarizer': LabelBinarizer,
+    'kbins_discretizer': KBinsDiscretizer
 }
 
 
